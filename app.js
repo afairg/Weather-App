fd174a356ea6cd1e593aaf55007051ff17b43c57
Avery <averymrich22@gmail.com>
2025-02-23 19:55:12 -0600

    add functionality for multiple locations

--- app.js
+++ app.js
@@ -1,13 +1,59 @@
 const weatherIconContainer = document.querySelector('#weatherIconContainerDiv');
 const strWeatherDescriptor = document.querySelector('#txtWeatherDescriptor');
-const strWeatherInfo = document.querySelector('#WeatherInfoDiv')
+const strWeatherInfo = document.querySelector('#weatherInfoDiv');
+const strCityState = document.querySelector('#txtCityState');
+
+
+// Function to retrieve the user's location using OpenStreetMap's Nominatim API
+async function getLocation() {
+    return new Promise((resolve, reject) => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(async function (position) {
+                try {
+                    const strLat = position.coords.latitude.toString();
+                    const strLong = position.coords.longitude.toString();
+                    let url = `https://nominatim.openstreetmap.org/reverse?lat=${strLat}&lon=${strLong}&format=json`;
+
+                    let objResponse = await fetch(url);
+                    let objData = await objResponse.json();
+                    let strCity = objData.address.city || objData.address.town || objData.address.village || "Unknown Location";
+                    let strState = objData.address.state;
+
+                    console.log("Location Data:", objData);
+
+                    resolve({
+                        lat: strLat,
+                        long: strLong,
+                        city: strCity,
+                        state: strState
+                    });
+
+                } catch (error) {
+                    console.error("Error retrieving city name:", error);
+                    reject(error);
+                }
+            },
+            (error) => {
+                console.error("Error retrieving user location data:", error);
+                reject(error);
+            });
+        } else {
+            console.log("Geolocation is not supported by this browser.");
+            reject(new Error("Geolocation not supported"));
+        }
+    });
+}
 
 // Function to retrieve and parse weather information from the Open-Meteo API
 async function getWeatherInfo() {
-    const strResponse = await fetch('https://api.open-meteo.com/v1/forecast?latitude=36.1628&longitude=-85.5016&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,weather_code,cloud_cover&daily=temperature_2m_max,temperature_2m_min,precipitation_probability_max&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FChicago&forecast_days=1');
+    const objLocation = await getLocation();
+    const strLat = objLocation.lat;
+    const strLong = objLocation.long;
+    const strCity = objLocation.city;
+    const strState = objLocation.state;
+    console.log("objLocation: ", objLocation);
+    const strResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${strLat}&longitude=${strLong}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,weather_code,cloud_cover&daily=temperature_2m_max,temperature_2m_min,precipitation_probability_max&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FChicago&forecast_days=1`);
     const strWeatherData = await strResponse.json();
-    console.log(strWeatherData);
-
     // Error case, data could not be retrieved
     if (strWeatherData.error) {
         Swal.fire({
@@ -16,17 +62,18 @@ async function getWeatherInfo() {
             text: 'An error occurred while trying to get the weather information. Please try again later.'
         })
     }
-
     // Return the current and daily weather data
     return {
         current: strWeatherData.current,
-        daily: strWeatherData.daily
+        daily: strWeatherData.daily,
+        city: strCity,
+        state: strState
     }
 }
 
 
 // Function to display the appropriate weather icon and descriptor based on the weather code
-function displayWeatherIcon(strWeatherData) {
+function displayWeatherImages(strWeatherData) {
     const strWeatherCode = strWeatherData.weather_code;
     // Clear weather
     if (strWeatherCode == '0') {
@@ -53,6 +100,11 @@ function displayWeatherIcon(strWeatherData) {
         strWeatherInfo.innerHTML = '<h2 class="text-center mb-0">Cloudy</h2>';
         weatherIconContainer.innerHTML = '<i class="bi bi-cloud icon-200"></i>';
     }
+    // Foggy weather
+    else if (strWeatherCode == '45' || strWeatherCode == '48') {
+        strWeatherInfo.innerHTML = '<h2 class="text-center mb-0">Fog</h2>';
+        weatherIconContainer.innerHTML = '<i class="bi bi-cloud-fog icon-200"></i>';
+    }
     //Rainy weather
     else if (strWeatherCode == '51' || strWeatherCode == '53' || strWeatherCode == '55' || strWeatherCode == '61' || strWeatherCode == '63' 
         || strWeatherCode == '65' || strWeatherCode == '80' || strWeatherCode == '81' || strWeatherCode == '82'
@@ -60,6 +112,19 @@ function displayWeatherIcon(strWeatherData) {
         strWeatherInfo.innerHTML = '<h2 class="text-center mb-0">Rain</h2>';
         weatherIconContainer.innerHTML = '<i class="bi bi-cloud-rain icon-200"></i>';
     }
+    //Stormy weather
+    else if (strWeatherCode == '95') {
+        strWeatherInfo.innerHTML = '<h2 class="text-center mb-0">Thunderstorms</h2>';
+        weatherIconContainer.innerHTML = '<i class="bi bi-cloud-lightning-rain icon-200"></i>';
+    }
+    //Snowy weather
+    else if (strWeatherCode == '71' || strWeatherCode == '73' || strWeatherCode == '75' || strWeatherCode == '77' || strWeatherCode == '85' 
+        || strWeatherCode == '86'
+    ) {
+        strWeatherInfo.innerHTML = '<h2 class="text-center mb-0">Snow</h2>';
+        weatherIconContainer.innerHTML = '<i class="bi bi-cloud-snow icon-200"></i>';
+    }
+    // "Unknown" weather
     else {
         weatherIconContainer.innerHTML = '<i class="bi bi-cloud-slash icon-200"></i>';
     }
@@ -69,8 +134,11 @@ function displayWeatherIcon(strWeatherData) {
 // Function to display weather information
 async function displayWeatherInfo() {
     const strWeatherData = await getWeatherInfo();
+    console.log("getWeatherInfo: ", strWeatherData);
     const strCurrentWeather = strWeatherData.current;
     const strDailyWeather = strWeatherData.daily;
+    const strCity = strWeatherData.city;
+    const strState = strWeatherData.state;
     const strTemperature = parseInt(strCurrentWeather.temperature_2m);
     const strHumidity = strCurrentWeather.relative_humidity_2m;
     const strApparentTemperature = parseInt(strCurrentWeather.apparent_temperature);
@@ -78,12 +146,13 @@ async function displayWeatherInfo() {
     const strLowTemperature = parseInt(strDailyWeather.temperature_2m_min);
     const strChancePrecipitation = parseInt(strDailyWeather.precipitation_probability_max);
     const strCloudCover = parseInt(strCurrentWeather.cloud_cover);
-    displayWeatherIcon(strWeatherData.current);
+    displayWeatherImages(strWeatherData.current);
 
-    // Display the current temperature
+    strCityState.innerText = `${strCity}, ${strState}`;
+    // Display the current weather conditions
     strWeatherInfo.innerHTML += `<p class="text-center temp-text mb-0">${strTemperature}°F</p>`;
     strWeatherInfo.innerHTML += `<div class="d-flex justify-content-around"><h5>High: ${strHighTemperature}°F</h5><h5>Low: ${strLowTemperature}°F</h5><h5 class="text-center">Feels like: ${strApparentTemperature}°F</h5></div>`;
-    strWeatherInfo.innerHTML += `<table class="table mt-4" id="weatherTable" aria-label="Weather Information">
+    strWeatherInfo.innerHTML += `<table class="table mt-4" id="weatherTable" aria-label="Weather Information Table">
                                     <tbody>
                                         <tr>
                                             <td>Humidity</td>
